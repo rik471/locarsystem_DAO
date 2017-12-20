42e462d969982c1f7d8370b08643be755d6b748f
rik471 <riksongleison471@gmail.com>
2017-12-20 01:57:09 -0200

    ok

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
   spl_autoload_register(function($class_name){
 
-    $filename = $class_name.".php";
-
+    $filename = str_replace ("\\", "/", "class" . DIRECTORY_SEPARATOR . $class_name .".php");
+    
     if (file_exists(($filename))) {
         require_once($filename);
     }
--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
   require_once("config.php");
 
-  $sql = new Banco();
+  /*$sql = new Banco();
 
   $sql->query("UPDATE CLIENTE SET CNH = B WHERE CODCLI = 1");
 
   $select = $sql->select("SELECT * FROM locarsystem.CLIENTE");
 
-  echo json_encode($select);
+  echo json_encode($select);*/
+
+  $user = new Cliente();
+
+  $user->carregarPorCliente(1);
+
+  echo $user;
  ?>
