7665dc457000caa0df728c1de4ecac39ca465cf7
rik471 <riksongleison471@gmail.com>
2017-12-20 00:37:33 -0200

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+  require_once("config.php");
+
+  $sql = new Banco();
+
+  $sql->query("UPDATE CLIENTE SET CNH = B WHERE CODCLI = 1");
+
+  $select = $sql->select("SELECT * FROM locarsystem.CLIENTE");
+
+  echo json_encode($select);
+ ?>
