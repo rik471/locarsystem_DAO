93aaed00121a1d379ed6c44b3d4d4022a42c58ff
rik471 <riksongleison471@gmail.com>
2017-12-23 22:59:39 -0200

    reviwsao de codigo

--- class/Cliente.php
+++ class/Cliente.php
@@ -4,6 +4,8 @@
 
     /*ATRIBUTOS*/
     private $codCli;
+    private $login;
+    private $senha;
     private $nome;
     private $telefone;
     private $endereco;
@@ -21,12 +23,32 @@
     }
 
 
+
+    public function getLogin(){
+      return $this->login;
+    }
+    public function setLogin($login){
+      $this->login = $login;
+    }
+
+
+
+    public function getSenha(){
+      return $this->senha;
+    }
+    public function setSenha($senha){
+      $this->senha = $senha;
+    }
+
+
+
     public function getNome(){
       return $this->nome;
     }
     public function setNome($nome){
       $this->nome = $nome;
     }
+
 
 
     public function getTelefone(){
@@ -37,12 +59,14 @@
     }
 
 
+
     public function getEndereco(){
       return $this->endereco;
     }
     public function setEndereco($endereco){
       $this->endereco = $endereco;
     }
+
 
 
     public function getTipoCli(){
@@ -53,12 +77,14 @@
     }
 
 
+
     public function getDataCadastro(){
       return $this->dataCadastro;
     }
     public function setDataCadastro($dataCadastro){
       $this->dataCadastro = $dataCadastro;
     }
+
 
 
     public function getCnh(){
@@ -79,47 +105,120 @@
       ));
 
       if (count($result) > 0) {
-
-        $row = $result[0];
-
-        $this->setCodCli($row['CODCLI']);
-        $this->setNome($row['NOME']);
-        $this->setTelefone($row['TELEFONE']);
-        $this->setEndereco($row['ENDERECO']);
-        $this->setTipoCli($row['TIPOCLI']);
-        $this->setDataCadastro(new DateTime($row['DATACADASTRO']));
-        $this->setCnh($row['CNH']);
-
+          $this->setDados($results[0]);
       }
     }
 
+    //FUNÇÃO PARA LISTAR USUARIOS
     public static function lista(){
 
       $sql = new Banco();
 
-      return $sql->select("SELECT * FROM CLIENTE ORDER BY CODCLI");
-
+      $results = $sql->select("SELECT * FROM locarsystem.CLIENTE ORDER BY LOGIN");
+      if (count($result) > 0) {
+          $this->setDados($results[0]);
+      }
     }
 
+    //FUNÇÃO PARA BUSCAR USUARIO POR LETRAS E PALAVRAS
     public static function busca($letra){
 
       $sql = new Banco();
 
-      return $sql->select("SELECT * FROM CLIENTE WHERE NOME LIKE :NOME ORDER BY NOME", array(
+      $results = $sql->select("SELECT * FROM locarsystem.CLIENTE WHERE NOME LIKE :NOME ORDER BY NOME", array(
         ':NOME'=>'%'.$letra.'%'
       ));
+          $this->setDados($results[0]);
     }
 
-    // FUNCAO PRA MOSTRAR NA TELA
+
+
+    //FUNÇÃO PARA AUTENTICAR CODIGO E SENHA
+    public function login($nome, $senha){
+
+      $sql = new Banco();
+
+      $results = $sql->select("SELECT * FROM locarsystem.CLIENTE WHERE LOGIN = :LOGIN AND SENHA = :SENHA", array(
+        ':LOGIN'=>$login,
+        ':SENHA'=>$senha
+      ));
+      if (count($results) > 0) {
+        $this->setDados($results[0]);
+      }else{
+        throw new Exception("Login ou Senha incorretos");
+      }
+    }
+
+    //INSERE NOVO USUARIO
+    public function insert(){
+
+      $sql = new Banco();
+
+      $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :SENHA)", array(
+        ":LOGIN"=>$this->getLogin(),
+        ":SENHA"=>$this->getSenha()
+      ));
+      if (count($results) > 0){
+        $this->setDados($results[0]);
+      }
+    }
+
+    ///UPDATE
+    public function update($login, $senha){
+
+       $this->setLogin($login);
+       $this->setSenha($senha);
+
+       $sql = new Banco();
+
+       $sql->query("UPDATE CLIENTE SET LOGIN = :LOGIN, SENHA = :SENHA WHERE CODCLI = :ID", array(
+         ":LOGIN"=>$this->getLogin(),
+         ":SENHA"=>$this->getSenha(),
+         ":ID"=>$this->getCodCli()
+       ));
+       if (count($results) > 0){
+         $this->setDados($results[0]);
+       }
+    }
+
+    //NOVO USUARIO PASSANDO LOGIN E SENHA NA CLASSE Cliente
+    public function __construct($login = "", $senha = ""){
+      $this->setLogin($login);
+      $this->setSenha($senha);
+    }
+
+    //METODO DE MOSTRAR DADOS QUANDO USANDO SETTERS
+    public function setDados($dados){
+
+      $this->setCodCli($dados['CODCLI']);
+      $this->setLogin($dados['LOGIN']);
+      $this->setSenha($dados['SENHA']);
+      $this->setNome($dados['NOME']);
+      $this->setTelefone($dados['TELEFONE']);
+      $this->setEndereco($dados['ENDERECO']);
+      $this->setTipoCli($dados['TIPOCLI']);
+      $this->setDataCadastro(new DateTime($dados['DATACADASTRO']));
+      $this->setCnh($dados['CNH']);
+
+      foreach ($dados as &$row) {
+          $row['ENDERECO'] = utf8_encode($row['ENDERECO']);
+      }
+          return $dados;
+
+    }
+
+    // FUNCAO PRA MOSTRAR NA TELA EM STRING COM JSON ENCODE
     public function __toString(){
 
       return json_encode(array(
         "CODCLI"=>$this->getCodCli(),
+        "LOGIN"=>$this->getLogin(),
+        "SENHA"=>$this->getSenha(),
         "NOME"=>$this->getNome(),
         "TELEFONE"=>$this->getTelefone(),
         "ENDERECO"=>$this->getEndereco(),
         "TIPOCLI"=>$this->getTipoCli(),
-        "DATACADASTRO"=>$this->getDataCadastro()->format("d/m/Y")
+        "DATACADASTRO"=>$this->getDataCadastro()->format("d/m/Y H:i:s")
       ));
     }
   }
--- index.php
+++ index.php
@@ -16,12 +16,37 @@
   echo $user;*/
 
   //BUSCANDO LISTA DE USUARIOS
-  $lista = Cliente::lista();
-  echo json_encode($lista);
+  //$lista = Cliente::lista();
+  //echo json_encode($lista);
 
-//busca por letras e silabas
+  /*$listar = new Banco();
+  $listar->select("SELECT * FROM CLIENTE;");
+  echo json_encode($listar);*/
 
-  $busca = Cliente::busca("r");
-
+  //busca por letras e silabas
+  $busca = Cliente::busca("ri");
   echo json_encode($busca);
+
+  //AUTENTICANDO LOGIN E SENHA
+  //$sql = new Cliente();
+  //$sql->login("Rick471", "rickvei");
+  //echo $sql;
+
+  //INSERINDO NOVO USUARIOS
+  /*$novo = new Cliente();
+  $novo->setLogin("PontoDuplo");
+  $novo->setSenha("master");
+  $novo->insert();
+  echo $novo;*/
+
+  //Inserindo novo usuario na classe Cliente
+  //$novo = new Cliente("RicardoVei", "Pardal");
+  //$novo->insert();
+  //echo $novo;
+
+  //UPDATE credenciais de usuario
+  /*$atualizar = new Cliente();
+  $atualizar->carregarPorCliente(1);
+  $atualizar->update("Rick880", "rickvei");
+  echo $atualizar;*/
  ?>
